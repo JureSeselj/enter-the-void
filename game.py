8b397126803fff0d19d0ec0b480c5c67afb9f671
JureSeselj <jure.seselj@gmail.com>
2022-09-11 13:57:02 +0000

    Create other dictionary containing strings for instructions and game introduction

--- game.py
+++ game.py
@@ -257,3 +257,52 @@ INTRO_DICTIONARY = {'1':
                     'Robo-Annihilator,\n'
                     'appears and starts to pull you in with its '
                     'tractor beam.\nWhat do you do?\n'}
+
+# Dictionary containing strings for intructions and game introductions.
+OTHER_DICT = {'instruct':
+              '\nIn Star Traveller, you must choose the name of your\n'
+              'captain, the name of your ship and choose a collection\nof '
+              'useful '
+              'items to hold in your cargo.\nYou will then have to navigate a '
+              'series of scenarios,\nchoosing to burn precious fuel,\nuse up '
+              'your '
+              'item collection or take increasingly dangerous risks to '
+              'progress.'
+              '\nIf you fail a scenario,\nyou will get a game over.\nThe game '
+              'is '
+              'played by inputting numbers or letters or\nstrings of text '
+              'when prompted by the text on screen.',
+              'intro':
+              '\n\nWelcome to Star Traveller!\n'
+              'You are a captain of the Star Republic Navy.\n'
+              'You have been tasked with delivering a superweapon\nfrom '
+              'Sector A to Sector E.\n'
+              'This will allow the Star Republic to defeat the Robo-Empire.\n'
+              'You will face many perils on your way there, but the Star '
+              'Republic\nis relying on you!'
+              '\n'}
+
+
+def retrieve_scenario_text(player_object, scenario, conclusion_number):
+    """
+    Function builds key and prints retrieved dictionary text.
+
+    Takes scenario and conclusion numbers, builds key, retrieves
+    string and replaces with attributes from the Player instance.
+    Then prints string.
+
+    Parameters:
+    player_object (Player): Provides attributes to replace parts of the
+    string text.
+    scenario (int): Provides first part of key for dictionary.
+    conclusion_number (int): Provides second part of key for dictionary.
+
+    Returns: Returns nothing but does print string.
+    """
+    key = str(scenario) + ',' + str(conclusion_number)
+    scenario_text = SCENARIO_DICTIONARY[key].replace('{player_object.name}',
+                                                     player_object.name)
+    scenario_text = scenario_text.replace('{player_object.ship_name}',
+                                          player_object.ship_name)
+    print(scenario_text)
+    return
