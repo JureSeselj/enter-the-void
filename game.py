71d97e8228c31e7e4fd5ac5647d44bb1dcb0d034
JureSeselj <jure.seselj@gmail.com>
2022-09-11 13:38:39 +0000

    Create game.py file which contains dictionaries of strings and one function

---
+++ game.py
@@ -0,0 +1,227 @@
+"""
+Module which contains dictionaries of strings and one
+function to access and change dictionary items.
+"""
+
+# Dictionary containing strings for scenario conclusions.
+SCENARIO_DICTIONARY = {'1,1':
+                       'Captain {player_object.name} uses extra fuel to '
+                       'accelerate '
+                       '{player_object.ship_name} out of the way\nbefore the '
+                       'asteroid storm hits.\nCaptain {player_object.name} '
+                       'makes the '
+                       'hyperspace jump to Sector B.',
+                       '1,3':
+                       'Captain {player_object.name} masterfully pilots '
+                       '{player_object.ship_name} through the asteroid storm,'
+                       '\n'
+                       'performing incredibly risky maneuvers that push their '
+                       'skills to the limit.\nHaving survived this, '
+                       'Captain {player_object.name} makes the hyperspace '
+                       'jump to '
+                       'Sector B.',
+                       '1,4':
+                       'Captain {player_object.name} activates the Temporary '
+                       'Force'
+                       ' Shield they took in the cargo.\nA kinetic barrier '
+                       'envelops {player_object.ship_name}, allowing it to\n'
+                       'safely traverse the asteroid storm.\nFeeling rather '
+                       'lucky; '
+                       'Captain {player_object.name} makes the hyperspace jump'
+                       ' to Sector B.',
+                       '1,5':
+                       'One after another, asteroids crash into the side of '
+                       '{player_object.ship_name}.\nThe hull is eventually '
+                       'breached and Captain {player_object.name} is left\nto '
+                       'the mercy of cold space.',
+                       '2,1':
+                       'Switching on {player_object.ship_name}\'s '
+                       'afterburners,\n'
+                       'Captain {player_object.name} burns some of the'
+                       'additional'
+                       '\nfuel to make sure they aren\'t\npulled into the '
+                       'black '
+                       'hole\'s event horizon.\nBreathing a sigh of relief, '
+                       'Captain\n{player_object.name} makes\nthe hyperspace '
+                       'jump to Sector C.',
+                       '2,3':
+                       'Captain {player_object.name} directs '
+                       '{player_object.ship_name} on a course\nso that it '
+                       'will '
+                       'approach the black hole\'s orbit at a '
+                       'tangent,\ncatapulting '
+                       'the ship out of the black hole\'s gravity well.\n'
+                       'With extreme speed, '
+                       '{player_object.ship_name} is launched across the '
+                       'solar '
+                       'system\nbut Captain {player_object.name}\'s quick '
+                       'thinking has saved them.\nCaptain '
+                       '{player_object.name} '
+                       'makes the hyperspace jump to Sector C.',
+                       '2,4':
+                       'Captain {player_object.name} switches on the '
+                       'Anti-Gravity '
+                       'Device\nin {player_object.ship_name}\'s cargo.\nThis '
+                       'causes '
+                       '{player_object.ship_name} to be propelled away from '
+                       'the '
+                       'black hole\ninstead of towards it.\nCaptain '
+                       '{player_object.name} makes the hyperspace jump to '
+                       'Sector C.',
+                       '2,5':
+                       'Captain {player_object.name} fails to stop '
+                       '{player_object.ship_name} from\nbeing pulled into the '
+                       'black hole.\nAs they pass the event horizon, '
+                       '{player_object.name} notices\ntime start to slow and\n'
+                       'gravity begin to get continually heavier and '
+                       'heavier...',
+                       '3,1':
+                       'Captain {player_object.name} uses '
+                       '{player_object.ship_name}\'s fuel reserves,\nflying '
+                       'it out '
+                       'of range of the aliens\' antiquated weapon systems.\n'
+                       'Captain {player_object.name} makes a safe hyperspace '
+                       'jump to Sector D.',
+                       '3,2':
+                       'Captain {player_object.name} tries to use '
+                       '{player_object.ship_name}\'s fuel reserves\nto fly '
+                       'out of '
+                       'range of the alien\' weapon systems,\nbut they find '
+                       'there '
+                       'is no more reserve.\n"If only I\'d taken more fuel" '
+                       'is the '
+                       'last thought to pass\ninto {player_object.name}\'s '
+                       'head before {player_object.ship_name} explodes.',
+                       '3,3':
+                       'Captain {player_object.name} realises '
+                       '{player_object.ship_name} is\nfar superior to the '
+                       'aliens\' '
+                       'ships.\nCaptain {player_object.name} powers up '
+                       '{player_object.ship_name}\'s weapon systems\nand '
+                       'fires '
+                       'before the aliens have a chance to power up their '
+                       'own,\n'
+                       'destroying the aliens.\nCaptain {player_object.name} '
+                       'makes the hyperspace jump to Sector D.',
+                       '3,4':
+                       'Turning on the Galactic Translator, Captain '
+                       '{player_object.name} speaks with the aliens.\nIt '
+                       'turns out '
+                       'they are the Bug People from Sector X, and have '
+                       'mistaken\n'
+                       'you for an agent of the Robo-Empire.\nAfter clearing '
+                       'up '
+                       'the misunderstanding, {player_object.name} makes a '
+                       'quick\nhyperspace jump to Sector D.',
+                       '3,5':
+                       'Captain {player_object.name} attempts to answer the '
+                       'transmission in all\nthe space languages they know,'
+                       '\nbut no '
+                       'answer comes back.\nThe aliens power up their\nweapon '
+                       'systems and their turbo lasers destroy '
+                       '{player_object.ship_name}.',
+                       '4,1':
+                       'Just in time, Captain {player_object.name} uses the '
+                       'fuel '
+                       'reserves to\nboost and hide {player_object.ship_name} '
+                       'behind a planet\'s rings,\nevading the blockade\'s '
+                       'sensors.\nCaptain {player_object.name} waits for '
+                       'the blockade to move before making\nthe hyperspace '
+                       'jump to Sector E.',
+                       '4,2':
+                       'Captain {player_object.name} attempts to move quickly '
+                       'to '
+                       'hide from the blockade\nbehind a planet\'s rings,'
+                       '\nbut just '
+                       'when they want to tap into the fuel reserves,\nthey '
+                       'realise there are none left.\nAfter several minutes, '
+                       '{player_object.ship_name} '
+                       'drifts into view of the blockade\n'
+                       'and is destroyed by the incoming missiles.',
+                       '4,3':
+                       'Captain {player_object.name} realises that they '
+                       'can\'t '
+                       'defeat the entire blockade by themselves,\nbut comes '
+                       'up '
+                       'with a cunning plan.\nThey wait to be targetted by '
+                       'the blockade\'s missiles, only to fly\n'
+                       '{player_object.ship_name} close to the blockade,'
+                       '\ncausing the missiles to '
+                       'destroy the blockade itself!\nCaptain '
+                       '{player_object.name} '
+                       'passes the destroyed blockade\nand hyperspace jumps '
+                       'to Sector E.',
+                       '4,4':
+                       'Thinking quickly, Captain {player_object.name} '
+                       'activates\n'
+                       'the Cloaking Device in the cargo hold.\n'
+                       'Suddenly, {player_object.ship_name} '
+                       'becomes invisible to the naked '
+                       'eye and to sensors.\nCaptain {player_object.name} '
+                       'moves the ship carefully past the blockade\nand '
+                       'hyperspace jumps to Sector E when out of their range.',
+                       '4,5':
+                       'A barrage of homing missiles\nfly towards '
+                       '{player_object.ship_name}.\n{player_object.name}\'s '
+                       'pilot '
+                       'skills are no match for the missiles\'s\n'
+                       'ability to turn on a whim,\n'
+                       'and {player_object.ship_name} is destroyed in '
+                       'a massive explosion.',
+                       '5,1':
+                       'Using the last of {player_object.ship_name}\'s fuel '
+                       'reserve,\nCaptain {player_object.name} causes the '
+                       'force of '
+                       'the ship to overpower\nthe Robo-Annihilator\'s '
+                       'tractor '
+                       'beam;\nescaping from the Robo-Annihilator.\nCaptain '
+                       '{player_object.name} delivers the superweapon to the '
+                       'Star '
+                       'Republic Navy,\nand after the war is '
+                       'won becomes Admiral {player_object.name}.',
+                       '5,2':
+                       'Captain {player_object.name} attempts to use the fuel '
+                       'reserve to escape the\ntractor beam\'s pull,\nbut '
+                       'realises '
+                       'there is no more reserve!\nThey are captured, '
+                       'the superweapon is lost,\nand so is the last hope of '
+                       'the Star Republic.',
+                       '5,3':
+                       'Captain {player_object.name} allows themselves to be '
+                       'taken aboard the Robo-Annihilator.\nInside, they pull '
+                       'out a concealed '
+                       'laser pistol and kill the Robo-Guards,\nrunning '
+                       'through '
+                       'the ship before activating its self-destruct button.\n'
+                       'Captain {player_object.name} '
+                       'boards {player_object.ship_name} just in time to '
+                       'escape '
+                       'the\nexploding Robo-Annihilator.\nAfterwards, they '
+                       'deliver '
+                       'the superweapon to the Star Republic Navy,\nbecoming '
+                       'Admiral {player_object.name} and winning the war '
+                       'against the Robo-Empire.',
+                       '5,4':
+                       'As {player_object.ship_name} is pulled by the tractor '
+                       'beam,\nCaptain {player_object.name} releases the '
+                       'Nuclear '
+                       'Mines from the cargo hold.\nThe mines are pulled into '
+                       'the '
+                       'hold of the Robo-Annihilator,\ncausing the '
+                       'dreadnought to explode in a mushroom cloud.'
+                       '\nThankfully, {player_object.ship_name} just escapes '
+                       'the destruction and after '
+                       'delivering\nthe superweapon to the Star Republic Navy,'
+                       '\nCaptain {player_object.name} became '
+                       'known as the hero \'{player_object.name} the Daring\''
+                       '\n'
+                       'for their exploits in destroying the '
+                       'Robo-Annihilator.',
+                       '5,5':
+                       'Captain {player_object.name} is imprisoned aboard the '
+                       'Robo-Annihilator.\nThe superweapon is taken and the '
+                       'last '
+                       'thing {player_object.name}\nsees before they are '
+                       'executed\n'
+                       'is the capital world of the Star Republic being '
+                       'destroyed by the superweapon.'}
