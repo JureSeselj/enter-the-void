240f9026fff2d04e8fd727fd688c85f4cd649cc5
JureSeselj <jure.seselj@gmail.com>
2022-09-11 14:23:49 +0000

    Add import in run.py file, also add global constant list of winning cargo

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Module which runs the game.
+"""
+
+# Imported for random integer generation
+import random
+# Imported to hold dictionaries of strings and one function to access them
+import game
+
+# Global constant list of winning cargo items in order
+WINNING_CARGO = ['Eclipse Shield Cygnus', 'Anti-Gravity Device',
+                 'Galaxy Transformer', 'Tianzhou Device', 'SpaceX Dragon Mines']
