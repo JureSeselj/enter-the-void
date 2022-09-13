fdad1b12dfc26fc6a28129a1fd2cb2af2359c64f
JureSeselj <jure.seselj@gmail.com>
2022-09-13 17:37:06 +0000

    Changed description for all functions in run.py file

--- run.py
+++ run.py
@@ -15,12 +15,9 @@ WINNING_CARGO = ['Eclipse Shield Cygnus', 'Anti-Gravity Device',
 def quit_out():
     """
     Quits application.
-    
     Thank the player for playing and
     then quits the application.
-    
     Parameters: No parameters.
-    
     Returns: No return values.
     """
     print('\nThanks for playing Enter The Void!')
@@ -30,15 +27,12 @@ def quit_out():
 def validate_start_menu_option():
     """
     Validates player choice at start menu.
-    
     Asks the player what they want to do
     at the start menu and then
     validates whether the player wants to
     play Enter The Void, see the instructions for
     playing or quit the game.
-    
     Parameters: No parameters.
-    
     Returns: String which represents a number of 1, 2 or 3.
     """
     while True:
@@ -58,12 +52,9 @@ def validate_start_menu_option():
 def main():
     """
     Sends the player where they want to go.
-    
     Calls the start screen and receives the players
     input then sends them where they want to go.
-    
     Parameters: No parameters.
-    
     Returns: No return values, but does call other functions.
     """
     while True:
@@ -80,7 +71,6 @@ def main():
 def instructions():
     """
     Gives the player instructions on how to play the game.
-    
     Parameters: No parameters.
     Returns: No return values. Just prints strings.
     """
@@ -90,13 +80,10 @@ def instructions():
 def introduction():
     """
     Introduction to the game.
-    
     Introduces the user to the game if
     they have chosen to play and then sends the
     player to create their own Player instance.
-    
     Parameters: No parameters.
-    
     Returns: No return values. Just prints strings
     and then calls function for player to get started on creating
     their own Player instance.
@@ -108,17 +95,14 @@ def introduction():
 def validate_initial_cargo_choices(potential_cargo):
     """
     Validates the initial cargo choices.
-    
     Asks the player to input a number and checks and
     validates if that number for the cargo is available.
     If not it creates a while loop where the player is
     asked to provide a valid number.
     Makes sure there are no value or index errors.
-    
     Parameters:
     potential_cargo (list of str): List of items the player
     can potentially still take.
-    
     Returns:
     number (int): Integer which is used to index the potential
     cargo items list outside the function.
@@ -139,13 +123,10 @@ def validate_initial_cargo_choices(potential_cargo):
 def validate_replay_choice():
     """
     Validates whether player wants to replay game.
-    
     Validates whether the player has chosen either 'Y' or 'N'
     for their choice in the replay function. Creates
     while loop so player must input correctly.
-    
     Parameters: No parameters.
-    
     Returns:
     replay_choice(chr): 'Y' or 'N' so that player can
     replay or quit game.
@@ -161,17 +142,14 @@ def validate_replay_choice():
 def validate_scenario_choice(player_object):
     """
     Validates the player's scenario choice.
-    
     Validates whether the player has inputted a
     possible scenario choice that is available to them.
     Creates while loop so that player must input
     a valid choice.
-    
     Parameters:
     player_object (Player): Used to work out the length of the
     list of cargo items so that the player's input can be assessed
     against the list of available inputs.
-    
     Returns:
     number (int): Returned so that the game can decide where to proceed.
     Whether the player has progressed to the next scenario, achieved victory
@@ -193,16 +171,13 @@ def validate_scenario_choice(player_object):
 def get_name(name_in_question):
     """
     Asks the player to name something in their Player instance.
-    
     Asks the user's name for captain or ship
     depending on argument provided and validates
     whether that is correct by creating a while loop and checking
     that the name is valid.
-    
     Parameters:
     name_in_question (str): Either the name of the player's
     captain or the spaceship's name.
-    
     Returns:
     name (str): Returns a string which is used to develop the Player
     instance. If the argument is the spaceship name then a 'the '
@@ -223,13 +198,10 @@ def get_name(name_in_question):
 def decide_on_items():
     """
     Asks the player to choose three items out of potential 5.
-    
     Asks player to choose items and presents a shrinking list
     as they pick more. Appends a new list that is used for the
     Player instance.
-    
     Parameters: No parameters.
-    
     Returns:
     cargo (list of str): Returns cargo list which is used to
     instantiate the Player instance for the game.
@@ -257,7 +229,6 @@ def decide_on_items():
 def scenario_conclusion(player_object, scenario, conclusion_number):
     """
     Function which acts as 'switch' to retrieve conclusion text.
-    
     Parameters:
     player_object (Player): Instance of Player class
     created by player at start of game.
@@ -265,7 +236,6 @@ def scenario_conclusion(player_object, scenario, conclusion_number):
     place currently in the game.
     conclusion_number (int): Integer to identify which conclusion
     has happened based on game logic.
-    
     Returns: Returns nothing.
     """
     print('\n')
@@ -334,14 +304,11 @@ def scenario_conclusion(player_object, scenario, conclusion_number):
 def replay():
     """
     Function which acts as 'switch' for replaying the game.
-    
     This function calls the validation function for
     the player's replay choice and then calls a function to
     go to the start menu or quit the game (and terminal)
     entirely.
-    
     Parameters: No parameters.
-    
     Returns: Returns nothing.
     """
     print('Would you like to play again?')
@@ -355,15 +322,12 @@ def replay():
 def game_over(player_object):
     """
     Function which is called when the player loses the game.
-    
     Prints game over text and allows them to
     quit or play again by calling
     replay function.
-    
     Parameters:
     player_object (Player): Uses this to print an fstring
     with the name of the captain inside.
-    
     Returns: Returns nothing.
     """
     print(f'\n\nCaptain {player_object.name} has died.')
@@ -373,15 +337,12 @@ def game_over(player_object):
 def victory(player_object):
     """
     Function which is called when the player wins the game.
-    
     Prints victory text and allows them to
     quit or play again by calling
     replay function.
-    
     Parameters:
     player_object (Player): Uses this to print an fstring
     with the name of the captain inside.
-    
     Returns: Returns nothing.
     """
     print(f'\n\nWell done Captain {player_object.name}. You have saved the '
@@ -392,16 +353,13 @@ def victory(player_object):
 def display_options(player_object):
     """
     Function which displays options for player in scenario.
-    
     Function which is called and displays options to the player based on
     their current cargo. Also displays Player class methods that the player
     can call upon.
-    
     Parameters:
     player_object (Player): Used to display the
     cargo list in the Player instance and to also display the
     fuel attribute.
-    
     Returns: Returns nothing.
     """
     counter = 1
@@ -421,13 +379,11 @@ def scenario_intro(number, player_object):
     is provided to the player depending on how far along the game they
     are. Calls from dictionary.Moves to victory function if player
     has completed all scenarios.
-    
     Parameters:
     number (int): Integer used to decide which intro should
     be called from dictionary.
     player_object (Player): Used to be passed on to the
     victory function if player has passed all scenarios.
-    
     Returns: Returns nothing.
     """
     if number == 1:
@@ -447,11 +403,8 @@ def scenario_intro(number, player_object):
 def move_on():
     """
     Function that pauses the game.
-    
     Waits for any input then continues when that input is delivered.
-    
     Parameters: No parameters.
-    
     Returns: Returns nothing.
     """
     input('\nPress enter to continue.\n')
@@ -460,13 +413,11 @@ def move_on():
 
 def scenario_call(player_object, scenario_number, risk_factor):
     """Function for calling a scenario for the player.
-    
     Calls itself when a scenario is
     completed successfully or calls game over.
     Does this by validating players scenario choice
     and checking it against things in the Player
     instance.
-    
     Parameters:
     player_object (Player): Used to pass on to
     other functions and to call class methods to see if they
@@ -476,7 +427,6 @@ def scenario_call(player_object, scenario_number, risk_factor):
     risk_factor (int): Factor used to be increased when passed
     into next scenario and to test against take_chance method
     in Player instance.
-    
     Returns: Returns nothing.
     """
     scenario_intro(int(scenario_number), player_object)
@@ -517,7 +467,6 @@ class Player:
     """
     This is a class that holds data and has methods for the
     player to navigate the game.
-    
     Attributes:
     name (str): The name of the Captain.
     ship_name (str): The name of the spaceship.
@@ -527,7 +476,6 @@ class Player:
     def __init__(self, name, ship_name, cargo):
         """
         The constructor for Player class.
-        
         Parameters:
         name (str): The name of the Captain.
         ship_name (str): The name of the spaceship.
@@ -542,9 +490,7 @@ class Player:
         """
         Function to remove 1 fuel and
         check if player has run out.
-        
         Parameters: No parameters.
-        
         Returns:
         bool: Returns True or False depending on
         whether fuel is greater than or equal to 0.
@@ -560,14 +506,11 @@ class Player:
     def take_chance(self, factor):
         """
         Decides whether player has survived risky action.
-        
         Generates random integer between 1 and 10 and
         then checks against factor to decide outcome.
-        
         Parameters:
         factor (int): Integer that increases throughout game.
         Is checked against random integer to decide outcome.
-        
         Returns:
         bool: True or False to decide if the
         player has survived the scenario or not.
@@ -582,17 +525,14 @@ class Player:
 def confirm_choice(question, details):
     """
     Function that asks the player if the specific detail is correct.
-    
     Before creating the Player instance in the main function. This function
     returns either True or False to break each while loop in the create_player
     function. Also validates player's answer.
-    
     Parameters:
     question (str): Specific question the player must answer yes
     or no to.
     details (str or list of str): Either name, ship name or cargo
     list player has chosen
-    
     Returns:
     bool: Either True or False to break while loop in
     create_player function.
@@ -612,9 +552,7 @@ def create_player():
     """
     Provides a series of questions which create the Player
     instance and then calls the scenario.
-    
     Parameters: No parameters.
-    
     Returns: Returns nothing just creates Player instance and
     calls scenario.
     """
